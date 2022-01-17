6af6ce7f79fb3d51ebdba38d2464ade1428627e8
we craft <moin@wecraft.agency>
2022-01-17 11:47:09 +0100

    changed arrow

--- template-parts/index/index-1.php
+++ template-parts/index/index-1.php
@@ -28,9 +28,9 @@ get_header();
                 $args = [
                     'headline' => 'Reise-Empfehlungen',
                     'text' => 'Travel is the movement of people between relatively distant geographical locations, and can involve travel by foot, bicycle, automobile, train, boat, bus, airplane, or other means, with or without luggage, and can be one way or round trip.',
-                    'link_top' => false,
-                    'link_bottom' => false,
-                    'link_teaser' => false,
+                    'link_top' => true,
+                    'link_bottom' => true,
+                    'link_teaser' => true,
                     'link_top_text' => 'Alle [TOTAL_RESULT] Reisen zum Thema anzeigen',
                     'link_bottom_text' => 'Alle [TOTAL_RESULT] Reisen zum Thema anzeigen',
                     'link_teaser_text' => 'Alle [TOTAL_RESULT] Reisen zum Thema anzeigen',
