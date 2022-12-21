d854a3afcbc5e2ad738f7d354e893e383ed45ea6
we craft <moin@wecraft.agency>
2022-12-21 03:52:10 +0100

    removed every own variable

--- footer.php
+++ footer.php
@@ -44,21 +44,22 @@
 
     <div class="footer-main--boxes">
         <div class="container">
-            <div class="col-12 col-sm-6 col-lg-3">
-                <div class="h5">
-                    Kontakt
-                </div>
-                <p>
-                    <strong><?php echo do_shortcode('[ts-company-name]');?></strong><br>
-                    <?php echo do_shortcode('[ts-company-zip]');?>
-                    <?php echo do_shortcode('[ts-company-city]');?>
-                </p>
-                <p>
-                    <a href="tel:<?php echo do_shortcode('[ts-company-hotline]');?>" title="<?php echo do_shortcode('[ts-company-name]');?> anrufen" class="icon-link">
-                        <div class="icon">
-                            <svg><use xmlns:xlink="http://www.w3.org/1999/xlink" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/iconoir-sprite.svg#phone-call"></use></svg>
-                        </div>
-                        <?php echo do_shortcode('[ts-company-hotline]');?>
+            <div class="row">
+                <div class="col-12 col-sm-6 col-lg-3">
+                    <div class="h5">
+                        Kontakt
+                    </div>
+                    <p>
+                        <strong><?php echo do_shortcode('[ts-company-name]');?></strong><br>
+                        <?php echo do_shortcode('[ts-company-zip]');?>
+                        <?php echo do_shortcode('[ts-company-city]');?>
+                    </p>
+                    <p>
+                        <a href="tel:<?php echo do_shortcode('[ts-company-hotline]');?>" title="<?php echo do_shortcode('[ts-company-name]');?> anrufen" class="icon-link">
+                    <div class="icon">
+                        <svg><use xmlns:xlink="http://www.w3.org/1999/xlink" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/iconoir-sprite.svg#phone-call"></use></svg>
+                    </div>
+                    <?php echo do_shortcode('[ts-company-hotline]');?>
                     </a>
 
                     <a href="mailto:<?php echo do_shortcode('[ts-company-mail]');?>" title="E-Mail an <?php echo do_shortcode('[ts-company-name]');?>" class="icon-link">
@@ -67,39 +68,40 @@
                         </div>
                         <?php echo do_shortcode('[ts-company-email]');?>
                     </a>
-                </p>
-            </div>
-            <div class="col-12 col-sm-6 col-lg-3">
-                <?php
-                $args = [
-                    'menu_location' => 'footer_column_1'
-                ];
-                load_template_transient(get_template_directory().'/template-parts/footer/menu.php', false, $args);
-                ?>
-            </div>
+                    </p>
+                </div>
+                <div class="col-12 col-sm-6 col-lg-3">
+                    <?php
+                    $args = [
+                        'menu_location' => 'footer_column_1'
+                    ];
+                    load_template_transient(get_template_directory().'/template-parts/footer/menu.php', false, $args);
+                    ?>
+                </div>
 
-            <div class="col-12 col-sm-6 col-lg-3">
-                <?php
-                $args = [
-                    'menu_location' => 'footer_column_2'
-                ];
-                load_template_transient(get_template_directory().'/template-parts/footer/menu.php', false, $args);
-                ?>
-                <div class="h5">
-                    Searchroutes
+                <div class="col-12 col-sm-6 col-lg-3">
+                    <?php
+                    $args = [
+                        'menu_location' => 'footer_column_2'
+                    ];
+                    load_template_transient(get_template_directory().'/template-parts/footer/menu.php', false, $args);
+                    ?>
+                    <div class="h5">
+                        Searchroutes
+                    </div>
+                    <?php
+                    // For Example purposes only, output all search routes
+                    echo do_shortcode('[ts-searchroutes]');
+                    ?>
                 </div>
-                <?php
-                // For Example purposes only, output all search routes
-                echo do_shortcode('[ts-searchroutes]');
-                ?>
-            </div>
-            <div class="col-12 col-sm-6 col-lg-3">
-                <div class="h5">
-                    <?php echo do_shortcode('[ts-company-name]');?>
+                <div class="col-12 col-sm-6 col-lg-3">
+                    <div class="h5">
+                        <?php echo do_shortcode('[ts-company-name]');?>
+                    </div>
+                    <p>
+                        <?php echo do_shortcode('[ts-company-name]');?>  möchte Menschen zusammenbringen, um gemeinsame unvergessliche, glückliche Reiseerlebnisse zu genießen.
+                    </p>
                 </div>
-                <p>
-                    <?php echo do_shortcode('[ts-company-name]');?>  möchte Menschen zusammenbringen, um gemeinsame unvergessliche, glückliche Reiseerlebnisse zu genießen.
-                </p>
             </div>
         </div>
     </div>
