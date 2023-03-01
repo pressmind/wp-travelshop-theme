01447dd97de851e3e6b45f72f15b33ad90047090
we craft <moin@wecraft.agency>
2023-03-01 18:09:29 +0100

    messenger as plain text icon, cuz sprite is not working with gradients

--- template-parts/micro-templates/link-sharing.php
+++ template-parts/micro-templates/link-sharing.php
@@ -1,6 +1,7 @@
 <?php
 $actual_link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
 // var_dump($actual_link);
+// @todo: build links to share pages with text
 
 if ( !isset ( $args['share_options'] ) ) {
     $args['share_options'] = [
