3cc6f0e107e0c1b7faf51a78133dc37092f0d9ae
we craft <moin@wecraft.agency>
2023-03-02 16:19:04 +0100

    added bolder check for checkboxes

--- template-parts/pm-views/detail-blocks/detail-head-info-bottom.php
+++ template-parts/pm-views/detail-blocks/detail-head-info-bottom.php
@@ -1,4 +1,8 @@
 <?php
+use Pressmind\HelperFunctions;
+use Pressmind\Travelshop\PriceHandler;
+use Pressmind\Travelshop\Template;
+
 /**
  * @var $args | mediaobject data
  */
@@ -9,6 +13,30 @@ if(empty($args['cheapest_price']) || !empty($args['booking_on_request'])){
 ?>
 
 <div class="detail-header-info-bottom">
+
+    <div class="detail-header-info-chosen-date">
+        <div class="detail-header-info-chosen-date-icon">
+            <?php echo Template::render(APPLICATION_PATH.'/template-parts/micro-templates/transport-icon.php', ['transport_type' => $args['cheapest_price']->transport_type]); ?>
+        </div>
+        <div class="detail-header-info-chosen-date-info">
+            <div class="detail-header-info-chosen-date-data">
+                <div class="small-title">
+                    Datum wählen
+                </div>
+                <div>
+                    <?php echo Template::render(APPLICATION_PATH.'/template-parts/micro-templates/travel-date-range.php', [
+                        'date_departure' => $args['cheapest_price']->date_departure,
+                        'date_arrival' => $args['cheapest_price']->date_arrival
+                    ]);?>
+                </div>
+            </div>
+
+            <button type="button" data-action="detail-change-date" class="detail-header-info-chosen-date-edit">
+                <svg><use xmlns:xlink="http://www.w3.org/1999/xlink" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/phosphor-sprite.svg#pencil"></use></svg>
+            </button>
+        </div>
+    </div>
+
     <?php // Random Availability
     $randint = random_int(1, 9);
     ?>
