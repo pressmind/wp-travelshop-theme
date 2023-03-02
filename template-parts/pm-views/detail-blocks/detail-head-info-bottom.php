ddd939161e6978ddbcb65a02b70d0c01ba9426cc
we craft <moin@wecraft.agency>
2023-03-02 16:13:10 +0100

    added bolder check for checkboxes

--- template-parts/pm-views/detail-blocks/detail-head-info-bottom.php
+++ template-parts/pm-views/detail-blocks/detail-head-info-bottom.php
@@ -9,6 +9,9 @@ if(empty($args['cheapest_price']) || !empty($args['booking_on_request'])){
 ?>
 
 <div class="detail-header-info-bottom">
+    <?php // Random Availability
+    $randint = random_int(1, 9);
+    ?>
     <div class="detail-header-info-cta">
         <?php echo Template::render(APPLICATION_PATH.'/template-parts/micro-templates/booking-button.php', [
             'cheapest_price' => $args['cheapest_price'],
@@ -16,5 +19,11 @@ if(empty($args['cheapest_price']) || !empty($args['booking_on_request'])){
             'modal_id' => $args['id_modal_price_box'],
             'disable_id' => true
         ]);?>
+    </div>
+    <div class="detail-header-info-status">
+        <?php if($randint < 10) { ?>
+            <!-- Toggle in badge the class "active" to toggle status with animation -->
+            <div class="status active <?php echo $randint <= 3 ? 'alert' : ''; ?>">Nur noch <?php echo $randint < 10 ? $randint == 1 ? '1 Platz' : $randint . ' Plätze ' : ''; ?> frei</div>
+        <?php } ?>
     </div>
 </div>
