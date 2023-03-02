e0b3cee75f4d382fab5de3e3a0dbaee3d773831c
we craft <moin@wecraft.agency>
2023-03-02 13:41:37 +0100

    added bolder check for checkboxes

--- assets/js/ui.js
+++ assets/js/ui.js
@@ -590,7 +590,8 @@ jQuery(function ($) {
 
         // -- backdrop checker
         if ( $target.css('container-name') === 'backdrop' ) {
-            console.log('is-backdrop element');
+
+            $target.parent().find('button[data-type="close-popup"]').trigger('click');
 
             // -- trigger click on data-type="popup-close", to hide item by klick on close button..
             // -- @todo: best way?
