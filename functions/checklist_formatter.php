9fcd56eb6abad604e1acf13260a99b6cc223de29
we craft <moin@wecraft.agency>
2023-03-03 13:18:23 +0100

    light color lighter

---
+++ functions/checklist_formatter.php
@@ -0,0 +1,9 @@
+<?php
+function checkmarkList($content) {
+
+    $iconClass = '';
+    $checkIcon = '<svg><use xmlns:xlink="http://www.w3.org/1999/xlink" href="/wp-content/themes/travelshop/assets/img/phosphor-sprite.svg#check-bold"></use></svg>';
+
+
+    return str_replace(['<ul>','<li>'], ['<ul class="checklist">','<li>'.$checkIcon], $content);
+}
