d2ee33c7822016a9124bbc29167af508cffe0de6
we craft <moin@wecraft.agency>
2023-03-03 13:06:49 +0100

    added bolder check for checkboxes

--- template-parts/pm-views/detail-blocks/detail-head-info-top.php
+++ template-parts/pm-views/detail-blocks/detail-head-info-top.php
@@ -8,14 +8,24 @@
 
     <?php
     if ( !empty($args['zielgebiete']) ) {
-        print_r($args['zielgebiete']);
+        ?>
+        <div class="detail-header-info-top-attributes">
+            <?php foreach ( $args['zielgebiete'] as $item ) { ?>
+                <a href="<?php echo $item['url']; ?>" title="<?php echo $item['name']; ?>" target="_blank">
+                    <?php echo $item['name']; ?>
+                </a>
+            <?php } ?>
+        </div>
+        <?php
     }
     ?>
-    <h2><?php echo $args['name']; ?></h2>
-    <?php if (!empty($args['subline'])) { ?>
-        <p><?php echo $args['subline']; ?></p>
-    <?php } ?>
-    <?php if (!empty($args['usps'])) { ?>
-        <?php echo $args['usps']; ?>
-    <?php } ?>
+    <div class="detail-header-info-top-body">
+        <h2><?php echo $args['name']; ?></h2>
+        <?php if (!empty($args['subline'])) { ?>
+            <p><?php echo $args['subline']; ?></p>
+        <?php } ?>
+        <?php if (!empty($args['usps'])) { ?>
+            <?php echo $args['usps']; ?>
+        <?php } ?>
+    </div>
 </div>
