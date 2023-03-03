6cdc36f893abee51447c08840fd33d0ebefbd083
we craft <moin@wecraft.agency>
2023-03-03 13:48:02 +0100

    added checklist formatter for usage in various elements

--- template-parts/pm-views/detail-blocks/detail-head-info-top.php
+++ template-parts/pm-views/detail-blocks/detail-head-info-top.php
@@ -6,6 +6,20 @@
 
 <div class="detail-header-info-top">
 
+    <div data-pm-id="<?php echo $args['id_media_object']; ?>"
+         data-pm-ot="<?php echo $args['id_object_type']; ?>"
+         data-pm-dr="<?php //echo !is_null($args['cheapest_price']) ? $args['cheapest_price']->date_arrival->format('Ymd') . '-' . $args['cheapest_price']->date_arrival->format('Ymd') : ''; ?>"
+         data-pm-du="<?php echo !is_null($args['cheapest_price']) ? $args['cheapest_price']->duration : ''; ?>"
+         class="add-to-wishlist">
+        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-heart" width="30"
+             height="30" viewBox="0 0 24 24" stroke-width="1.5" stroke="#06f" fill="none"
+             stroke-linecap="round" stroke-linejoin="round">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+            <path class="wishlist-heart"
+                  d="M19.5 13.572l-7.5 7.428l-7.5 -7.428m0 0a5 5 0 1 1 7.5 -6.566a5 5 0 1 1 7.5 6.572"/>
+        </svg>
+    </div>
+
     <?php
     if ( !empty($args['destination_attributes']) || !empty($args['travel_type_attributes']) ) {
         ?>
