8f80f3a0ce0f0d8d0e6dbc9e86ea6da4381462ce
we craft <moin@wecraft.agency>
2023-03-04 16:24:33 +0100

    own variables for person / booking categry tree dropdowns

--- assets/js/search.js
+++ assets/js/search.js
@@ -111,11 +111,11 @@ jQuery(function ($) {
         });
     }
 
-// ------------------------------------------
-// --- rangeslider
-// ------------------------------------------
+    // ------------------------------------------
+    // --- rangeslider
+    // ------------------------------------------
 
-    if (false && $('.js-range-slider').length > 0) {
+    if ($('.js-range-slider').length > 0) {
         var rSliderElement = new rSlider({
             target: '#js-range-slider',
             values: { min: parseInt($(".js-range-slider").attr('data-min')), max: parseInt($(".js-range-slider").attr('data-max'))},
