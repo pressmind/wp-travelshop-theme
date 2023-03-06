c3d5134ae045065c7047622ac923c3111c8665e6
we craft <moin@wecraft.agency>
2023-03-06 15:11:52 +0100

    change order + define status row

--- template-parts/pm-views/detail-blocks/contact-box.php
+++ template-parts/pm-views/detail-blocks/contact-box.php
@@ -4,6 +4,38 @@
  */
 ?>
 
+<div class="detail-box detail-box--contact">
+    <div class="detail-box-body">
+        <div class="detail-contact-wrapper">
+            <a class="hotline-link" href="tel:<?php echo do_shortcode('[ts-company-hotline]');?>">
+                    <span class="hotline-icon">
+                        <svg><use xmlns:xlink="http://www.w3.org/1999/xlink" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/phosphor-sprite.svg#phone-call"></use></svg>
+                    </span>
+
+                <div class="hotline-info">
+                    <div class="hotline-title">
+                        <?php echo do_shortcode('[ts-company-hotline-info]'); ?>
+                    </div>
+                    <div class="hotline-number">
+                        <?php echo do_shortcode('[ts-company-hotline]');?>
+                    </div>
+                    <div class="hotline-openings">
+                        <?php
+                        $opening_times = wpsf_get_setting('travelshop_wpsf', 'contact_hotline', 'ts-company-opening-info');
+
+                        foreach ( $opening_times as $opening ) {
+                            echo "<div class='hotline-openings-item'>";
+                            echo $opening['sub-text'];
+                            echo "</div>";
+                        }
+                        ?>
+                    </div>
+                </div>
+
+            </a>
+        </div>
+    </div>
+</div>
 <div class="box-contact">
     <div class="box-contact-phone">
         <strong>Persönliche Beratung</strong><br />
