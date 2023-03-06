f35b6938d8015515258f6c883be46405c5ef0fe6
we craft <moin@wecraft.agency>
2023-03-06 15:15:43 +0100

    change order + define status row

--- template-parts/pm-views/detail-blocks/contact-box.php
+++ template-parts/pm-views/detail-blocks/contact-box.php
@@ -36,7 +36,7 @@
             </a>
 
             <a class="whatsapp-link" href="https://wa.me/<?php echo do_shortcode('[ts-company-hotline]');?>" target="_blank">
-                <svg><use xmlns:xlink="http://www.w3.org/1999/xlink" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/phosphor-sprite.svg#whatsapp"></use></svg>
+                <svg><use xmlns:xlink="http://www.w3.org/1999/xlink" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/phosphor-sprite.svg#whatsapp-logo"></use></svg>
 
                 WhatsApp
             </a>
