c05419b6a5e37b25732ec3c99d5f326371f595d5
we craft <moin@wecraft.agency>
2023-03-08 14:46:15 +0100

    detail boxes

--- template-parts/micro-templates/slider-controls.php
+++ template-parts/micro-templates/slider-controls.php
@@ -1,6 +1,6 @@
 <div class="slider-controls">
 
-    <button class="prev-button" >
+    <button class="prev-button" disabled="disabled">
         <svg><use xmlns:xlink="http://www.w3.org/1999/xlink" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/phosphor-sprite.svg#caret-left"></use></svg>
     </button>
     <button class="next-button" >
