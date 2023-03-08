642ab08cb0f97b703f9c7638c74c0af3c29cd8ca
we craft <moin@wecraft.agency>
2023-03-08 19:26:49 +0100

    detal overlay gallery

--- functions/remove_empty_paragraphs.php
+++ functions/remove_empty_paragraphs.php
@@ -1,43 +1,12 @@
 <?php
 function remove_empty_paragraphs($string) {
+    echo "<pre>";
+        print_r($string);
+    echo "</pre>";
 
-    $output = '';
+    $stringParts = explode('<p>', $string);
 
-    // every double space to single space
-    $string = str_replace('  ', ' ', $string);
-    $string = str_replace( '<p></p>', '', $string);
-    $string = str_replace( '<p> </p>', '', $string);
-    $string = str_replace( '<p>&nbsp;</p>', '', $string);
-
-    echo "hi";
-    // split into parts
-    $flags = PREG_SPLIT_DELIM_CAPTURE | PREG_SPLIT_NO_EMPTY;
-    $regex = '/(<[a-z0-9=\-:." ^\/]+\/>)|(<[^\/]+>[^<\/]+<\/[a-z0-9]+>)|(<[a-z0-9=\-:." ^\/]+>)/';
-    $parts = preg_split( $regex, $string, -1, $flags);
-
-    print_r($parts);
-
-    // -- re arrange parts
-    foreach ( $parts as $part ) {
-        $thisPart = trim($part);
-        $thisPart = preg_replace( '#<p>\s*</p>#', '', $thisPart );
-        $thisPart = preg_replace('#<p>(\s|&nbsp;)*+(<br\s*/*>)?(\s|&nbsp;)*</p>#i', '', $thisPart);
-
-        // check for empty paragraphs
-        $checkParagraphs = str_replace(['<p>', '</p>'], ['', ''], $thisPart);
-
-        if ( !empty($checkParagraphs) ) {
-            print_r('IS NOT EMPTY');
-        } else {
-            print_r('IS EMPTY');
-        }
-
-        print_r($thisPart);
-
-        if ( !empty($thisPart) ) {
-            $output .= $thisPart;
-        }
-    }
-
-    return $output;
+    echo "<pre>";
+    print_r($stringParts);
+    echo "</pre>";
 }
