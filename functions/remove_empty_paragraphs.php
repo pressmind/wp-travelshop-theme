e0fe613952f2ee6c8ca482e4b035b47628028ee6
we craft <moin@wecraft.agency>
2023-03-08 19:33:00 +0100

    detal overlay gallery

--- functions/remove_empty_paragraphs.php
+++ functions/remove_empty_paragraphs.php
@@ -9,17 +9,14 @@ function remove_empty_paragraphs($string) {
     $stringParts = explode('<p>', str_replace('</p>', '', $string));
 
     foreach ( $stringParts as $part ) {
-        echo "<pre>";
-        var_dump($part);
-        echo "</pre>";
-        if ( substr($part, 0, 1) === '<' ) {
-            $output .= $part;
-        } else {
-            $output .= "<p>" . $part ."</p>";
+        if ( !empty($part) && $part !== '' ) {
+            if ( substr($part, 0, 1) === '<' ) {
+                $output .= $part;
+            } else {
+                $output .= "<p>" . $part ."</p>";
+            }
         }
     }
 
-    echo "<pre>";
-    print_r($output);
-    echo "</pre>";
+    return $output;
 }
