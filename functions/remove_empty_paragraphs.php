51b0fc05c848e167a75d1cb3f9bf73347da86a62
we craft <moin@wecraft.agency>
2023-03-08 19:24:07 +0100

    detal overlay gallery

--- functions/remove_empty_paragraphs.php
+++ functions/remove_empty_paragraphs.php
@@ -5,6 +5,7 @@ function remove_empty_paragraphs($string) {
 
     // every double space to single space
     $string = str_replace('  ', ' ', $string);
+    $string = str_replace( '<p></p>', '', $string);
 
     // split into parts
     $flags = PREG_SPLIT_DELIM_CAPTURE | PREG_SPLIT_NO_EMPTY;
