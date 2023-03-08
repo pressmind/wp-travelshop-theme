406efc604172a302e23350f39db2016c33bfd962
we craft <moin@wecraft.agency>
2023-03-08 19:11:54 +0100

    detal overlay gallery

--- functions/remove_empty_paragraphs.php
+++ functions/remove_empty_paragraphs.php
@@ -1,19 +1,7 @@
 <?php
 function remove_empty_paragraphs($string) {
     // -- regex pattern to remove empty paragraphs
-    $output = preg_replace( array(
-        '#<p>\s*<(div|aside|section|article|header|footer)#',
-        '#</(div|aside|section|article|header|footer)>\s*</p>#',
-        '#</(div|aside|section|article|header|footer)>\s*<br ?/?>#',
-        '#<(div|aside|section|article|header|footer)(.*?)>\s*</p>#',
-        '#<p>\s*</(div|aside|section|article|header|footer)#',
-    ), array(
-        '<$1',
-        '</$1>',
-        '</$1>',
-        '<$1$2>',
-        '</$1',
-    ), $string );
+    $output = preg_replace( '#<p>\s*</p>#', '', $string );
 
     return preg_replace('#<p>(\s|&nbsp;)*+(<br\s*/*>)?(\s|&nbsp;)*</p>#i', '', $output);
 }
