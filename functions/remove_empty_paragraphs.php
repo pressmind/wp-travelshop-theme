ecd473d8ca27f0d90554d07082c79178b759dc57
we craft <moin@wecraft.agency>
2023-03-08 18:15:16 +0100

    detal overlay gallery

--- functions/remove_empty_paragraphs.php
+++ functions/remove_empty_paragraphs.php
@@ -2,5 +2,9 @@
 function remove_empty_paragraphs($string) {
     $pattern = "/<p[^>]*><\\/p[^>]*>/";
 
+    // -- easy replacement
+    $string = str_replace("<p></p>", "", $string);
+
+    // -- return with used regular expression $pattern
     return preg_replace($pattern, '', $string);
 }
