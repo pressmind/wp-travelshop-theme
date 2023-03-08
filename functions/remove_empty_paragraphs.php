7350b2bcbf300d748e10470f87504633476de9a7
we craft <moin@wecraft.agency>
2023-03-08 19:08:02 +0100

    detal overlay gallery

--- functions/remove_empty_paragraphs.php
+++ functions/remove_empty_paragraphs.php
@@ -1,15 +1,19 @@
 <?php
 function remove_empty_paragraphs($string) {
-    $string = "<h1>   </h1>" . $string;
-
     // -- regex pattern to remove empty paragraphs
-    $pattern = "/<[^\/>]*>([\s]?)*<\/[^>]*>/";
+    $output = $content = preg_replace( array(
+        '#<p>\s*<(div|aside|section|article|header|footer)#',
+        '#</(div|aside|section|article|header|footer)>\s*</p>#',
+        '#</(div|aside|section|article|header|footer)>\s*<br ?/?>#',
+        '#<(div|aside|section|article|header|footer)(.*?)>\s*</p>#',
+        '#<p>\s*</(div|aside|section|article|header|footer)#',
+    ), array(
+        '<$1',
+        '</$1>',
+        '</$1>',
+        '<$1$2>',
+        '</$1',
+    ), $string );
 
-    $return = trim($string);
-
-    // -- easy replacement
-    $return = str_replace("<p></p>", "", $return);
-
-    // -- return with used regular expression $pattern
-    return preg_replace($pattern, '', $return);
+    return preg_replace('#<p>(\s|&nbsp;)*+(<br\s*/*>)*(\s|&nbsp;)*</p>#i', '', $output);
 }
