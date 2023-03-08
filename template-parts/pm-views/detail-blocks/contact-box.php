83755a3908776de65efca64917d14f6190112bd5
we craft <moin@wecraft.agency>
2023-03-08 11:02:54 +0100

    detail boxes

--- template-parts/pm-views/detail-blocks/contact-box.php
+++ template-parts/pm-views/detail-blocks/contact-box.php
@@ -12,34 +12,40 @@
     </div>
     <div class="detail-box-body">
         <div class="detail-contact-wrapper">
-            <a class="hotline-link" href="tel:<?php echo do_shortcode('[ts-company-hotline]');?>">
+            <div class="detail-contact-hotline">
+                <a class="hotline-link" href="tel:<?php echo do_shortcode('[ts-company-hotline]');?>">
                 <span class="hotline-icon">
                     <svg><use xmlns:xlink="http://www.w3.org/1999/xlink" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/phosphor-sprite.svg#phone-call"></use></svg>
                 </span>
 
-                <div class="hotline-info">
-                    <div class="hotline-number">
-                        <?php echo do_shortcode('[ts-company-hotline]');?>
+                    <div class="hotline-info">
+                        <div class="hotline-number">
+                            <?php echo do_shortcode('[ts-company-hotline]');?>
+                        </div>
                     </div>
-                    <div class="hotline-openings">
-                        <?php
-                        $opening_times = wpsf_get_setting('travelshop_wpsf', 'contact_hotline', 'ts-company-opening-info');
+                </a>
 
-                        foreach ( $opening_times as $opening ) {
-                            echo "<div class='hotline-openings-item'>";
-                            echo $opening['sub-text'];
-                            echo "</div>";
-                        }
-                        ?>
-                    </div>
+
+                <div class="hotline-openings">
+                    <?php
+                    $opening_times = wpsf_get_setting('travelshop_wpsf', 'contact_hotline', 'ts-company-opening-info');
+
+                    foreach ( $opening_times as $opening ) {
+                        echo "<div class='hotline-openings-item'>";
+                        echo $opening['sub-text'];
+                        echo "</div>";
+                    }
+                    ?>
                 </div>
-            </a>
+            </div>
 
-            <a class="whatsapp-link" href="https://wa.me/<?php echo do_shortcode('[ts-company-hotline]');?>" target="_blank">
-                <svg><use xmlns:xlink="http://www.w3.org/1999/xlink" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/phosphor-sprite.svg#whatsapp-logo"></use></svg>
+            <div class="detail-contact-whatsapp">
+                <a class="whatsapp-link" href="https://wa.me/<?php echo do_shortcode('[ts-company-hotline]');?>" target="_blank">
+                    <svg><use xmlns:xlink="http://www.w3.org/1999/xlink" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/phosphor-sprite.svg#whatsapp-logo"></use></svg>
 
-                WhatsApp
-            </a>
+                    WhatsApp
+                </a>
+            </div>
         </div>
     </div>
 </div>
