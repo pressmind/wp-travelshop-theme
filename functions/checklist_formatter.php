175d2c5c23334ce1ab6bfb9deae6f9d9ec081ea1
we craft <moin@wecraft.agency>
2023-03-08 14:12:42 +0100

    detail boxes

--- functions/checklist_formatter.php
+++ functions/checklist_formatter.php
@@ -5,10 +5,9 @@ function checklist_formatter($content, $responsive = false) {
     $checkIcon = '<svg><use xmlns:xlink="http://www.w3.org/1999/xlink" href="/wp-content/themes/travelshop/assets/img/phosphor-sprite.svg#check-bold"></use></svg>';
     $checkListClass = 'checklist';
 
-    print_r($responsive);
 
     if ( $responsive ) {
-        $checkListClass .= ' checklist-responsive';
+        $checkListClass = 'checklist checklist-responsive';
     }
 
     return str_replace(['<ul>','<li>'], ['<ul class="checklist">','<li>'.$checkIcon], $content);
