e16c42044decaaa1ca4a183968c0b527e9a6f03a
we craft <moin@wecraft.agency>
2023-03-08 19:12:59 +0100

    detal overlay gallery

--- functions/remove_empty_paragraphs.php
+++ functions/remove_empty_paragraphs.php
@@ -1,7 +1,10 @@
 <?php
 function remove_empty_paragraphs($string) {
     // -- regex pattern to remove empty paragraphs
-    $output = preg_replace( '#<p>\s*</p>#', '', $string );
-
-    return preg_replace('#<p>(\s|&nbsp;)*+(<br\s*/*>)?(\s|&nbsp;)*</p>#i', '', $output);
+    $fix_empty_paragraphs = array(
+        '<p>['    => '[',
+        ']</p>'   => ']',
+        ']<br />' => ']'
+    );
+    return strtr( $string, $fix_empty_paragraphs );
 }
