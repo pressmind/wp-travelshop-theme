56674c35c993473f6a23c6ca9ab8d8fb5ebf6867
we craft <moin@wecraft.agency>
2023-03-08 19:14:41 +0100

    detal overlay gallery

--- functions/remove_empty_paragraphs.php
+++ functions/remove_empty_paragraphs.php
@@ -1,5 +1,14 @@
 <?php
 function remove_empty_paragraphs($string) {
+
+    $flags = PREG_SPLIT_DELIM_CAPTURE | PREG_SPLIT_NO_EMPTY;
+    $regex = '/(<[a-z0-9=\-:." ^\/]+\/>)|(<[^\/]+>[^<\/]+<\/[a-z0-9]+>)|(<[a-z0-9=\-:." ^\/]+>)/';
+    $parts = preg_split( $regex, $string, -1, $flags);
+
+    echo "<pre>";
+        print_r($parts);
+    echo "</pre>";
+
     // -- regex pattern to remove empty paragraphs
     $output = preg_replace( '#<p>\s*</p>#', '', $string );
 
