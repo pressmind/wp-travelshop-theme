d2be6ce9406d7ec2d45eb201874752e62053f7c8
we craft <moin@wecraft.agency>
2023-03-08 14:27:27 +0100

    detail boxes

--- template-parts/micro-templates/slider-controls.php
+++ template-parts/micro-templates/slider-controls.php
@@ -1,13 +1,9 @@
 <div class="slider-controls">
 
     <button class="prev-button" >
-        <div class="icon icon-xl">
-            <svg><use xmlns:xlink="http://www.w3.org/1999/xlink" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/phosphor-sprite.svg#caret-left"></use></svg>
-        </div>
+        <svg><use xmlns:xlink="http://www.w3.org/1999/xlink" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/phosphor-sprite.svg#caret-left"></use></svg>
     </button>
     <button class="next-button" >
-        <div class="icon icon-xl">
-            <svg><use xmlns:xlink="http://www.w3.org/1999/xlink" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/phosphor-sprite.svg#caret-right"></use></svg>
-        </div>
+        <svg><use xmlns:xlink="http://www.w3.org/1999/xlink" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/phosphor-sprite.svg#caret-right"></use></svg>
     </button>
 </div>
