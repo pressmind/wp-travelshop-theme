d316b1dcf8a5f50b9bfe03c29b2f47e6ce4c2677
we craft <moin@wecraft.agency>
2023-03-08 19:16:22 +0100

    detal overlay gallery

--- functions/remove_empty_paragraphs.php
+++ functions/remove_empty_paragraphs.php
@@ -1,16 +1,22 @@
 <?php
 function remove_empty_paragraphs($string) {
 
+    $output = '';
+
     $flags = PREG_SPLIT_DELIM_CAPTURE | PREG_SPLIT_NO_EMPTY;
     $regex = '/(<[a-z0-9=\-:." ^\/]+\/>)|(<[^\/]+>[^<\/]+<\/[a-z0-9]+>)|(<[a-z0-9=\-:." ^\/]+>)/';
     $parts = preg_split( $regex, $string, -1, $flags);
 
-    echo "<pre>";
-        print_r($parts);
-    echo "</pre>";
+    // -- re arrange parts
+    foreach ( $parts as $part ) {
+        $thisPart = trim($part);
+        $thisPart = preg_replace( '#<p>\s*</p>#', '', $thisPart );
+        $thisPart = preg_replace('#<p>(\s|&nbsp;)*+(<br\s*/*>)?(\s|&nbsp;)*</p>#i', '', $thisPart);
 
-    // -- regex pattern to remove empty paragraphs
-    $output = preg_replace( '#<p>\s*</p>#', '', $string );
+        if ( !empty($thisPart) ) {
+            $output .= $thisPart;
+        }
+    }
 
-    return preg_replace('#<p>(\s|&nbsp;)*+(<br\s*/*>)?(\s|&nbsp;)*</p>#i', '', $output);
+    return $output;
 }
