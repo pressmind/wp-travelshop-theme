1f738d579cbd77d389365777a76d3a46a4c7dc88
we craft <moin@wecraft.agency>
2023-03-08 18:28:43 +0100

    detal overlay gallery

--- functions/remove_empty_paragraphs.php
+++ functions/remove_empty_paragraphs.php
@@ -1,11 +1,13 @@
 <?php
 function remove_empty_paragraphs($string) {
+    // -- regex pattern to remove empty paragraphs
     $pattern = "/<p[^>]*><\\/p[^>]*>/";
 
+    $return = trim($string);
+
     // -- easy replacement
-    $string = trim($string);
-    $string = str_replace("<p></p>", "", $string);
+    $return = str_replace("<p></p>", "", $return);
 
     // -- return with used regular expression $pattern
-    return preg_replace($pattern, '', $string);
+    return preg_replace($pattern, '', $return);
 }
