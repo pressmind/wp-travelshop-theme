7ebb8d952b4fa8258849827bad0eec18ef57d617
we craft <moin@wecraft.agency>
2023-03-09 19:11:30 +0100

    changed class namings on various parts

--- assets/js/ui.js
+++ assets/js/ui.js
@@ -452,13 +452,13 @@ jQuery(function ($) {
             storeThisHeight = $(this).height();
 
             // -- set initiator class
-            if ( storeInnerHeight > storeThisHeight ) {
+            if ( storeInnerHeight >= storeThisHeight ) {
                 if ( !$(this).hasClass(teaserBlockNotToggleableClass) ) {
-                    $(this).removeClass(teaserBlockNotToggleableClass);
+                    $(this).addClass(teaserBlockNotToggleableClass);
                 }
             } else {
                 if ( $(this).hasClass(teaserBlockNotToggleableClass) ) {
-                    $(this).addClass(teaserBlockNotToggleableClass);
+                    $(this).removeClass(teaserBlockNotToggleableClass);
                 }
             }
         })
