3e79fd111fb9d4abb25b967ebbac5268de5f42b7
we craft <moin@wecraft.agency>
2023-03-09 19:27:29 +0100

    changed class namings on various parts

--- template-parts/pm-views/detail-blocks/mobile-bar.php
+++ template-parts/pm-views/detail-blocks/mobile-bar.php
@@ -15,9 +15,7 @@ if(empty($args['cheapest_price'])){
     <div class="container">
         <div class="detail-mobile-bar-row d-flex  flex-row justify-content-between align-items-center flex-nowrap">
             <div class="detail-mobile-bar-info">
-                <div class="detail-mobile-bar-info--title mx-0 my-0 h4">
-                    <?php echo $args['headline'] ?>
-                </div>
+                <div class="detail-mobile-bar-info--title mx-0 my-0 h4" title="<?php echo trim($args['headline']); ?>"><?php echo trim($args['headline']); ?></div>
 
                 <?php
                 if ( !empty($args['destination_attributes']) || !empty($args['travel_type_attributes']) ) {
