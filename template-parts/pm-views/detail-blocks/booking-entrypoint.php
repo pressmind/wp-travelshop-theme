8f0e1b5bbd246600d138b5ed9a85a79600ac3a30
we craft <moin@wecraft.agency>
2023-03-10 16:54:48 +0100

    added booking-entrypoint js

--- template-parts/pm-views/detail-blocks/booking-entrypoint.php
+++ template-parts/pm-views/detail-blocks/booking-entrypoint.php
@@ -41,10 +41,6 @@ if(empty($args['cheapest_price']) || !empty($args['booking_on_request'])){
         </div>
     </div>
 
-    <pre>
-        <?php echo print_r($args['cheapest_price']); ?>
-    </pre>
-
     <input class="booking-filter-field--duration" type="hidden" name="dur" value="<?php echo $args['cheapest_price']->duration; ?>" />
     <input class="booking-filter-field--mediaobject" type="hidden" name="mediaobject" value="<?php echo $args['cheapest_price']->id_media_object; ?>" />
     <input class="booking-filter-field--offer" type="hidden" name="offer" value="<?php echo $args['cheapest_price']->id; ?>" />
