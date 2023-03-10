658cc6816dc331832c9cb70e0ccf28139bfb1a1d
we craft <moin@wecraft.agency>
2023-03-10 16:22:32 +0100

    added booking-entrypoint js

--- assets/js/booking-entrypoint.js
+++ assets/js/booking-entrypoint.js
@@ -2,11 +2,19 @@ jQuery(function ($) {
 
     let bookingEntryTransportType = $('.booking-filter-radio--transport-type input[radio]');
     let bookingEntryAirport = $('.booking-filter-item--airport');
+    let bookingEntryAirportInput = bookingEntryAirport.find('input');
+
+    // -- remove attribute checked by click airport
+    bookingEntryAirportInput.on('click touch', function(e) {
+        bookingEntryAirportInput.removeAttribute('checked');
+    });
 
     // -- switch travel-type
     bookingEntryTransportType.on('click touch', function(e) {
 
         var thisType = $(this).val();
+
+        bookingEntryTransportType.removeAttribute('checked');
 
         if ( thisType == 'FLUG' ) {
             bookingEntryAirport.removeClass('d-none');
