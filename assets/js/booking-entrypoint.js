84bcaac8dbc8244461b2068af63b6d821c536cc4
we craft <moin@wecraft.agency>
2023-03-10 16:31:22 +0100

    added booking-entrypoint js

--- assets/js/booking-entrypoint.js
+++ assets/js/booking-entrypoint.js
@@ -1,20 +1,33 @@
 jQuery(function ($) {
+    let bookingEntry = $('.detail-booking-entrypoint');
 
-    // -- transport type / airport behaviour
-    let bookingEntryTransportType = $('.booking-filter-radio--transport-type input[type="radio"]');
-    let bookingEntryAirport = $('.booking-filter-item--airport');
+    // -- check if booking entry is loaded, otherwise script not needed
 
-    // -- switch travel-type
-    bookingEntryTransportType.on('change', function(e) {
-        var thisType = $(this).val();
+    if ( bookingEntry.length > 0 ) {
+        // -- transport type / airport behaviour
+        let bookingEntryTransportType = $('.booking-filter-radio--transport-type input[type="radio"]');
+        let bookingEntryAirport = $('.booking-filter-item--airport');
 
-        if ( thisType == 'FLUG' ) {
-            bookingEntryAirport.removeClass('d-none');
-        } else {
-            bookingEntryAirport.addClass('d-none');
-        }
-    });
+        // -- switch travel-type
+        bookingEntryTransportType.on('change', function(e) {
+            var thisType = $(this).val();
 
-    // -- on click calendar
+            if ( thisType == 'FLUG' ) {
+                bookingEntryAirport.removeClass('d-none');
+            } else {
+                bookingEntryAirport.addClass('d-none');
+            }
+        });
 
+        // -- on click calendar
+        let bookingEntryClendar = $('.booking-filter-field--date-range');
+
+        bookingEntryCalendar.on('click touch', function(e) {
+            e.preventDefault();
+
+            console.log('clicked booking-calendar');
+
+            e.stopPropagation();
+        })
+    }
 });
