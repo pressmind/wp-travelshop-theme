9093eb05221aafc52119a1047138a5abec2e1b81
we craft <moin@wecraft.agency>
2023-03-13 19:49:37 +0100

    added slider index, to show calendar active item on initialize

--- template-parts/micro-templates/booking-button.php
+++ template-parts/micro-templates/booking-button.php
@@ -24,11 +24,11 @@ use Pressmind\Travelshop\IB3Tools;
    <?php if(isset($args['disable_id']) && $args['disable_id'] === false) { ?> data-id-offer="<?php echo $args['cheapest_price']->getId(); ?>" <?php } ?>
    data-anchor="<?php echo $args['cheapest_price']->id; ?>" >
 
-    zur Buchung
+    <span>zur Buchung</span>
 
     <svg><use xmlns:xlink="http://www.w3.org/1999/xlink" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/phosphor-sprite.svg#caret-right-bold"></use></svg>
 
-<!--    <img class="loader"-->
-<!--         src="--><?php //echo WEBSERVER_HTTP; ?><!--/wp-content/themes/travelshop/assets/img/loading-dots.svg">-->
+    <img class="loader"
+         src="<?php echo WEBSERVER_HTTP; ?>/wp-content/themes/travelshop/assets/img/loading-dots.svg">
 </a>
 
