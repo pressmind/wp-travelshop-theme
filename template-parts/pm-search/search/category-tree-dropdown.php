9296c1a06e95a34ffde7560793bcc00d658afe59
we craft <moin@wecraft.agency>
2023-07-12 17:37:00 +0200

    changed mediaobject to mediaobjectid for better read

--- template-parts/pm-search/search/category-tree-dropdown.php
+++ template-parts/pm-search/search/category-tree-dropdown.php
@@ -25,55 +25,52 @@ if (empty($args['categories'][$args['fieldname']][0]) === false) {
             </button>
             <?php // @todo: did some changes to quellcode to use different view types for dropdowns ?>
             <div class="category-tree-field-dropdown">
-                <div class="dropdown-menu-inner">
-                    <div class="dropdown-menu-content">
-                        <div class="dropdown-menu-header d-none">
-                            <div class="h4">
-                                <?php echo $args['name']; ?>
-                            </div>
-                            <button class="filter-prompt" data-type="close-popup" type="button">
-                                <svg><use xmlns:xlink="http://www.w3.org/1999/xlink" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/phosphor-sprite.svg#x"></use></svg>
-                            </button>
+                <div class="category-tree-field-items-dropdown-header d-flex flex-row flex-nowrap justify-content-between">
+                    <div class="h5 mb-0">
+                        <?php echo $args['name']; ?>
+                    </div>
 
-                        </div>
-                        <div class="category-tree-field-items multi-level-checkboxes">
-                            <input type="hidden" name="<?php echo $args['fieldname'];?>-behavior" value="<?php echo empty($args['behavior']) ? 'OR' : $args['behavior']; ?>">
-                            <?php
-                            $childs = [];
-                            if (!empty($args['categories'][$args['fieldname']][1])) {
-                                foreach ($args['categories'][$args['fieldname']][1] as $item) {
-                                    $childs[$item->id_parent][] = $item;
-                                }
-                            }
-                            foreach ($args['categories'][$args['fieldname']][0] as $item) {
-                                $uuid = 'ti-' . uniqid();
-                                $has_childs = !empty($childs[$item->id_item]) && count($childs[$item->id_item]) > 1;
-                                ?>
-                                <div class="form-check <?php echo $has_childs ? 'has-second-level' : ''; ?>">
-                                    <input class="form-check-input" type="checkbox"
-                                           data-id-parent="" data-id="<?php echo $item->id_item; ?>"
-                                           data-name="<?php echo $args['fieldname']; ?>"
-                                           id="<?php echo $uuid; ?>"
-                                        <?php echo in_array($item->id_item, $selected) ? 'checked' : ''; ?>>
+                    <button type="button" class="close-category-dropdown d-flex d-lg-none">
+                        <svg><use xmlns:xlink="http://www.w3.org/1999/xlink" href="https://development-stanglmeier-de.pressmind.dev/wp-content/themes/travelshop/assets/img/phosphor-sprite.svg#x"></use></svg>
+                    </button>
+                </div>
+                <div class="category-tree-field-items multi-level-checkboxes">
+                    <input type="hidden" name="<?php echo $args['fieldname'];?>-behavior" value="<?php echo empty($args['behavior']) ? 'OR' : $args['behavior']; ?>">
+                    <?php
+                    $childs = [];
+                    if (!empty($args['categories'][$args['fieldname']][1])) {
+                        foreach ($args['categories'][$args['fieldname']][1] as $item) {
+                            $childs[$item->id_parent][] = $item;
+                        }
+                    }
+                    foreach ($args['categories'][$args['fieldname']][0] as $item) {
+                        $uuid = 'ti-' . uniqid();
+                        $has_childs = !empty($childs[$item->id_item]) && count($childs[$item->id_item]) > 1;
+                        ?>
+                        <div class="form-check <?php echo $has_childs ? 'has-second-level' : ''; ?>">
+                            <input class="form-check-input" type="checkbox"
+                                   data-id-parent="" data-id="<?php echo $item->id_item; ?>"
+                                   data-name="<?php echo $args['fieldname']; ?>"
+                                   id="<?php echo $uuid; ?>"
+                                <?php echo in_array($item->id_item, $selected) ? 'checked' : ''; ?>>
 
-                                    <span>
+                            <span>
                                                 <svg><use xmlns:xlink="http://www.w3.org/1999/xlink" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/phosphor-sprite.svg#check-bold"></use></svg>
                                             </span>
 
-                                    <label class="form-check-label" for="<?php echo $uuid; ?>">
-                                        <?php echo $item->name; ?>
-                                    </label>
-                                </div>
-                                <?php
-                            }
-                            ?>
+                            <label class="form-check-label" for="<?php echo $uuid; ?>">
+                                <?php echo $item->name; ?>
+                            </label>
                         </div>
-                        <div class="dropdown-menu-footer">
-                            <button class="btn btn-primary btn-block mt-3 filter-prompt">
-                                Auswahl übernehmen
-                            </button>
-                        </div>
-                    </div>
+                        <?php
+                    }
+                    ?>
+                </div>
+
+                <div class="category-tree-field-items-dropdown-footer">
+                    <button class="btn btn-primary btn-block mt-3 filter-prompt">
+                        Auswahl übernehmen
+                    </button>
                 </div>
             </div>
         </div>
