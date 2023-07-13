e6f7ac95e49e288471f02277d08685e6cecf086d
we craft <moin@wecraft.agency>
2023-07-13 15:00:04 +0200

    added search fields to travelshop-settings.php

--- assets/js/ui.js
+++ assets/js/ui.js
@@ -442,12 +442,12 @@ jQuery(function ($) {
 
             console.log(curScrollPosition);
             console.log(detailMobileBarThreshold);
-            console.log(detailContentThreshold);
 
             // fix to not show mobile bar if content is scroll through
             var detailContentPosition = $('.detail-section-content').offset().top;
             var detailContentHeight = $('.detail-section-content').height();
             var detailContentThreshold = detailContentPosition + detailContentHeight - $(window).height();
+            console.log(detailContentThreshold);
 
             if ( curScrollPosition > detailMobileBarThreshold && curScrollPosition < detailContentThreshold ) {
                 detailMobileBar.addClass('show');
