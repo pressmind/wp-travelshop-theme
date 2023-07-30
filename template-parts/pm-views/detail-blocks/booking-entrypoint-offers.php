1c5498438cf7e51c70783bdad41474ace02bf7f7
we craft <moin@wecraft.agency>
2023-07-30 16:12:39 +0200

    changes calendar

--- template-parts/pm-views/detail-blocks/booking-entrypoint-offers.php
+++ template-parts/pm-views/detail-blocks/booking-entrypoint-offers.php
@@ -37,7 +37,7 @@ $offers = $args['media_object']->getCheapestPrices($filter, ['date_departure' =>
         ?>
         <div class="booking-offer-item">
             <label for="offer<?php echo $offer->id; ?>">
-                <input type="radio" name="offer" id="offer<?php echo $offer->id; ?>" <?php echo ($activeOffer) ? 'checked="checked"' : ''; ?> />
+                <input type="radio" name="offer" id="offer<?php echo $offer->id; ?>" <?php echo (!$activeOffer) ? '' : 'checked="checked"'; ?> />
 
                 <div class="booking-offer-item-inner">
                     <?php echo Template::render(APPLICATION_PATH.'/template-parts/micro-templates/travel-date-range.php', [
