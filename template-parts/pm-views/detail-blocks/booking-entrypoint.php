636d39546632cc5cffa159f2c721402849f11add
we craft <moin@wecraft.agency>
2023-07-30 16:06:37 +0200

    changes calendar

--- template-parts/pm-views/detail-blocks/booking-entrypoint.php
+++ template-parts/pm-views/detail-blocks/booking-entrypoint.php
@@ -252,6 +252,7 @@ if(empty($args['cheapest_price']) || !empty($args['booking_on_request'])){
 
             <div class="booking-filter-item--dates">
                 <?php echo Template::render(APPLICATION_PATH.'/template-parts/pm-views/detail-blocks/booking-entrypoint-offers.php', [
+                    'media_object' => $args['media_object'],
                     'cheapest_price' => $args['cheapest_price'],
                     'date_departure' => $args['cheapest_price']->date_departure,
                     'date_arrival' => $args['cheapest_price']->date_arrival,
