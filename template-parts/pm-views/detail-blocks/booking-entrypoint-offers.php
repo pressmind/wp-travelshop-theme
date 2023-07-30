4aa780d5f820cf9b744d87ef21c619200f50cd1c
we craft <moin@wecraft.agency>
2023-07-30 16:16:39 +0200

    changes calendar

--- template-parts/pm-views/detail-blocks/booking-entrypoint-offers.php
+++ template-parts/pm-views/detail-blocks/booking-entrypoint-offers.php
@@ -22,6 +22,23 @@ $filter->occupancies_disable_fallback = false;
  * @var \Pressmind\ORM\Object\CheapestPriceSpeed[] $offers
  */
 $offers = $args['media_object']->getCheapestPrices($filter, ['date_departure' => 'ASC', 'price_total' => 'ASC'], [0, 100]);
+
+$date_to_cheapest_price = [];
+foreach($offers as $offer){
+    if($offer->duration != $args['cheapest_price']->duration) {
+        continue;
+    }
+    // if the date has multiple prices, display only the cheapest
+    if (!empty($date_to_cheapest_price[$offer->date_departure->format('Y-m-j')]) &&
+        $offer->price_total < $date_to_cheapest_price[$offer->date_departure->format('Y-m-j')]->price_total
+    ) {
+        // set the cheapier price
+        $date_to_cheapest_price[$offer->date_departure->format('Y-m-j')] = $offer;
+    } elseif (empty($date_to_cheapest_price[$offer->date_departure->format('Y-m-j')])
+    ){
+        $date_to_cheapest_price[$offer->date_departure->format('Y-m-j')] = $offer;
+    }
+}
 ?>
 
 <div class="booking-offer-items">
@@ -31,9 +48,24 @@ $offers = $args['media_object']->getCheapestPrices($filter, ['date_departure' =>
         if ( $offer->id === $args['offer_id'] ) {
             $activeOffer = true;
         }
+
+        $current_date = $offer->date_departure->format('Y-m-d');
+
+        $priceHTML = '';
+
+        if (($discount = PriceHandler::getDiscount($date_to_cheapest_price[$current_date])) !== false) {
+            $priceHTML = Template::render(APPLICATION_PATH.'/template-parts/micro-templates/discount.php', [
+                'cheapest_price' => $date_to_cheapest_price[$current_date],
+                'discount' => $discount,
+            ]);
+        } else {
+            $priceHTML = Template::render(APPLICATION_PATH.'/template-parts/micro-templates/price.php', [
+                'cheapest_price' => $date_to_cheapest_price[$current_date],
+            ]);
+        }
         ?>
         <div class="booking-offer-item">
-            <label for="offer<?php echo $offer->id; ?>">
+            <label data-price-html='<?php echo $priceHTML; ?>' for="offer<?php echo $offer->id; ?>" class="booking-offer-item-label" data-offer-id="<?php echo $offer->id; ?>">
                 <input type="radio" name="offer" id="offer<?php echo $offer->id; ?>" <?php echo ($activeOffer) ? 'checked="checked"' : ''; ?> />
 
                 <div class="booking-offer-item-inner">
