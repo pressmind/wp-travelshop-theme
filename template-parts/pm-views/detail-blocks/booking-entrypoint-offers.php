55881127cc7ae94ec9b53ff820ea216db96650b7
we craft <moin@wecraft.agency>
2023-07-30 16:04:12 +0200

    changes calendar

--- template-parts/pm-views/detail-blocks/booking-entrypoint-offers.php
+++ template-parts/pm-views/detail-blocks/booking-entrypoint-offers.php
@@ -17,9 +17,6 @@ if(empty($args['cheapest_price']) || !empty($args['booking_on_request'])){
 // build a date to best price map
 $filter = new CheapestPrice();
 $filter->occupancies_disable_fallback = false;
-if ( !empty($args['filter']['pm-tr']) ) {
-    $filter->transport_types = [$args['filter']['pm-tr']];
-}
 
 /**
  * @var \Pressmind\ORM\Object\CheapestPriceSpeed[] $offers
