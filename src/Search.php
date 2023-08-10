95d3cd456b8f1a44c66c83969ea255dd52e48717
we craft <moin@wecraft.agency>
2023-08-10 15:52:49 +0200

    changes calendar

--- src/Search.php
+++ src/Search.php
@@ -35,24 +35,25 @@ class Search
      *            ]
      *           ];
      * </code>
+     * @param int[] $allowed_visibilities e.g. 30 = public, 60 = hidden
      * @return array
      * @throws \Exception
      */
-    public static function getResult($request, $occupancy = 2, $page_size = 12, $getFilters = false, $returnFiltersOnly = false, $ttl_filter = null, $ttl_search = null, $output = null, $preview_date = null, $custom_conditions = [])
+    public static function getResult($request, $occupancy = 2, $page_size = 12, $getFilters = false, $returnFiltersOnly = false, $ttl_filter = null, $ttl_search = null, $output = null, $preview_date = null, $custom_conditions = [], $allowed_visibilities = [30])
     {
-        $allowed_visibilities = [30];
         $cache_key = md5(serialize(func_get_args()));
         if(isset(self::$_run_time_cache_full[$cache_key])){
             return self::$_run_time_cache_full[$cache_key];
         }
         $duration_filter_ms = null;
         $duration_search_ms = null;
-        if (empty($request['pm-ho']) === true) {
+        if (empty($request['pm-ho']) === true && !empty($occupancy)) {
             // if the price duration slider is active, we have to set the housing occupancy search
             // (to display the correct search result with the correct cheapest price inside)
             $request['pm-ho'] = $occupancy;
         }
         $id_object_type = empty($request['pm-ot']) ? false : \BuildSearch::extractObjectType($request['pm-ot']);
+        $order = empty($request['pm-o']) ? 'price-asc' : $request['pm-o'];
         if($getFilters){
             $FilterCondition = [];
             if(!empty($request['pm-ot'])){
@@ -63,7 +64,9 @@ class Search
             if(defined('TS_SEARCH_GROUP_KEYS') && !empty(TS_SEARCH_GROUP_KEYS)){
                 $FilterCondition[] = new \Pressmind\Search\Condition\MongoDB\Group(TS_SEARCH_GROUP_KEYS);
             }
-            $FilterCondition[] = new \Pressmind\Search\Condition\MongoDB\Occupancy($occupancy);
+            if(!empty($occupancy)){
+                $FilterCondition[] = new \Pressmind\Search\Condition\MongoDB\Occupancy($occupancy);
+            }
             $FilterCondition = array_merge($FilterCondition, $custom_conditions);
             $filter = new \Pressmind\Search\MongoDB($FilterCondition, ['price_total' => 'asc'], TS_LANGUAGE_CODE);
             $start_time = microtime(true);
@@ -94,41 +97,59 @@ class Search
                 $item['id_media_object'] = $document['id_media_object'];
                 $item['id_object_type'] = $document['id_object_type'];
                 $item['url'] = $document['url'];
-                $item['recommendation_rate'] = $document['recommendation_rate'];
+                $item['recommendation_rate'] = !empty($document['recommendation_rate']) ? $document['recommendation_rate'] : null;
                 $item['dates_per_month'] = [];
-                if (!empty($document['dates_per_month'])) {
-                    $item['dates_per_month'] = $document['dates_per_month'];
-                    foreach ($document['dates_per_month'] as $k => $month) {
-                        foreach ($month['five_dates_in_month'] as $k1 => $date) {
-                            $item['dates_per_month'][$k]['five_dates_in_month'][$k1]['date_departure'] = new \DateTime($date['date_departure']);
-                            $item['dates_per_month'][$k]['five_dates_in_month'][$k1]['date_arrival'] = new \DateTime($date['date_arrival']);
-                        }
-                    }
-                }
+                $item['fst_date_departure'] = !empty($document['fst_date_departure']) ? new \DateTime($document['fst_date_departure']) : null;
                 $item['possible_durations'] = !empty($document['possible_durations']) ? $document['possible_durations'] : [];
                 $item['last_modified_date'] = $document['last_modified_date'];
+                $item['sales_priority'] = !empty($document['sales_priority']) ? $document['sales_priority'] : null;
                 if (!empty($document['prices'])) {
                     if(!is_array($document['prices']['date_departures'])){
                         $document['prices']['date_departures'] = [$document['prices']['date_departures']];
                     }
                     $item['cheapest_price'] = new \stdClass();
                     $item['cheapest_price']->duration = $document['prices']['duration'];
-                    $item['cheapest_price']->price_total = $document['prices']['price_total'];
+                    $item['cheapest_price']->price_total = (float)$document['prices']['price_total'];
                     $item['cheapest_price']->price_regular_before_discount = $document['prices']['price_regular_before_discount'];
                     $item['cheapest_price']->earlybird_discount = $document['prices']['earlybird_discount'];
                     $item['cheapest_price']->earlybird_discount_f = $document['prices']['earlybird_discount_f'];
+                    $item['cheapest_price']->earlybird_name = empty($document['prices']['earlybird_name']) ? null : $document['prices']['earlybird_name'];
                     $item['cheapest_price']->date_departures = [];
                     if (!empty($document['prices']['date_departures'])) {
                         foreach ($document['prices']['date_departures'] as $date_departure) {
                             $item['cheapest_price']->date_departures[] = new \DateTime($date_departure);
                         }
                     }
-                    $item['cheapest_price']->earlybird_discount_date_to = new \DateTime($document['prices']['earlybird_discount_date_to']);
+                    $item['cheapest_price']->earlybird_discount_date_to = $document['prices']['earlybird_discount_date_to'] != null ? new \DateTime($document['prices']['earlybird_discount_date_to']) : null;
                     $item['cheapest_price']->option_board_type = $document['prices']['option_board_type'];
                     $item['cheapest_price']->transport_type = $document['prices']['transport_type'];
                 } else {
                     $item['cheapest_price'] = null;
                     $document['prices'] = null;
+                }
+                if (!empty($document['dates_per_month'])) {
+                    $document['dates_per_month'] = array_filter($document['dates_per_month'], function($item) {
+                        return !empty($item['five_dates_in_month']);
+                    });
+                    $document['dates_per_month'] = array_values($document['dates_per_month']);
+                    $item['dates_per_month'] = $document['dates_per_month'];
+                    foreach ($document['dates_per_month'] as $k => $month) {
+                        foreach ($month['five_dates_in_month'] as $k1 => $date) {
+                            $item['dates_per_month'][$k]['five_dates_in_month'][$k1]['date_departure'] = new \DateTime($date['date_departure']);
+                            $item['dates_per_month'][$k]['five_dates_in_month'][$k1]['date_arrival'] = new \DateTime($date['date_arrival']);
+                            $item['dates_per_month'][$k]['five_dates_in_month'][$k1]['duration'] = $date['duration'];
+                            $item['dates_per_month'][$k]['five_dates_in_month'][$k1]['price_total'] = (float)$date['price_total'];
+                            $item['dates_per_month'][$k]['five_dates_in_month'][$k1]['price_regular_before_discount'] = $date['price_regular_before_discount'];
+                            $item['dates_per_month'][$k]['five_dates_in_month'][$k1]['durations_from_this_departure'] = $date['durations_from_this_departure'];
+                            $item['dates_per_month'][$k]['five_dates_in_month'][$k1]['active'] = false;
+                            if(!empty($document['fst_date_departure']) && $item['dates_per_month'][$k]['five_dates_in_month'][$k1]['date_departure']->format('Y-m-d') === $item['fst_date_departure']->format('Y-m-d')){
+                                $item['dates_per_month'][$k]['five_dates_in_month'][$k1]['active'] = true;
+                            }
+                            if(!empty($item['cheapest_price']->price_total) && empty($document['fst_date_departure']) && $item['dates_per_month'][$k]['five_dates_in_month'][$k1]['price_total'] === $item['cheapest_price']->price_total){
+                                $item['dates_per_month'][$k]['five_dates_in_month'][$k1]['active'] = true;
+                            }
+                        }
+                    }
                 }
                 $item['departure_date_count'] = $document['departure_date_count'];
                 $item['possible_durations'] = !empty($document['possible_durations']) ? $document['possible_durations'] : [];
@@ -172,21 +193,50 @@ class Search
 
         $board_types = [];
         if(!empty($result_filter->boardTypesGrouped)){
-            $boardTypes = json_decode(json_encode($result_filter->boardTypesGrouped));
-            foreach($boardTypes as $item){
-                    $tmp = new \stdClass();
-                    $tmp->name = $item->_id;
-                    $board_types[]= $tmp;
+            $matching_board_types_map = [];
+            if(!$returnFiltersOnly){
+                $matching_board_types = json_decode(json_encode($result->boardTypesGrouped));
+                foreach($matching_board_types as $item){
+                    if(empty($item->_id)){
+                        continue;
+                    }
+                    $matching_board_types_map[$item->_id] = $item;
+                }
+            }
+            foreach(json_decode(json_encode($result_filter->boardTypesGrouped)) as $item){
+                if(empty($item->_id)){
+                    continue;
+                }
+                $item->count_in_system = $item->count;
+                $item->count_in_search = 0;
+                $item->name = $item->_id;
+                if(isset($matching_board_types_map[$item->_id])){
+                    $item->count_in_search = $matching_board_types_map[$item->_id]->count;
+                }
+                $board_types[$item->_id] = $item;
             }
         }
 
         $transport_types = [];
         if(!empty($result_filter->transportTypesGrouped)){
-            $transportTypes = json_decode(json_encode($result_filter->transportTypesGrouped));
-            foreach($transportTypes as $item){
-                $tmp = new \stdClass();
-                $tmp->name = $item->_id;
-                $transport_types[]= $tmp;
+            $matching_transport_types_map = [];
+            if(!$returnFiltersOnly){
+                $matching_transport_types = json_decode(json_encode($result->transportTypesGrouped));
+                foreach($matching_transport_types as $item){
+                    $matching_transport_types_map[$item->_id] = $item;
+                }
+            }
+            foreach(json_decode(json_encode($result_filter->transportTypesGrouped)) as $item){
+                if(empty($item->_id)){
+                    continue;
+                }
+                $item->count_in_system = $item->count;
+                $item->count_in_search = 0;
+                $item->name = $item->_id;
+                if(isset($matching_transport_types_map[$item->_id])){
+                    $item->count_in_search = $matching_transport_types_map[$item->_id]->count;
+                }
+                $transport_types[$item->_id] = $item;
             }
         }
 
@@ -223,6 +273,7 @@ class Search
                 'info' => []
             ],
             'id_object_type' => !$id_object_type ? [] : $id_object_type,
+            'order' => $order,
             'categories' => $categories,
             'board_types' => $board_types,
             'transport_types' => $transport_types,
