5d47381a24c7d28d470c5d9ad72c7679e2b2c91a
we craft <moin@wecraft.agency>
2023-08-10 15:55:55 +0200

    changes calendar

--- template-parts/micro-templates/date-dropdown.php
+++ template-parts/micro-templates/date-dropdown.php
@@ -16,6 +16,9 @@ use Pressmind\Travelshop\Template;
 $today = new DateTime();
 $date_format = $args['date_departures'][0]->format('Y') == $today->format('Y') ? 'd.m.' : 'd.m.Y';
 ?>
+<pre>
+    <?php print_r($args['dates_per_month']); ?>
+</pre>
 <div class="dropdown">
     <button class="btn <?php echo $args['departure_date_count'] == 1  || empty($args['dates_per_month']) ? ' disabled' : ' dropdown-toggle'; ?>"
             type="button"
