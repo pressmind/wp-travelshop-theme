0a0159abe09fae5c2058836f79b84ad0f1dd2f2a
we craft <moin@wecraft.agency>
2023-08-14 14:18:27 +0200

    changes calendar

--- functions/blog_search.php
+++ functions/blog_search.php
@@ -15,7 +15,7 @@ add_filter('get_search_form', function($form) {
 	<form role="search" method="get" class="form-string-search input-group mw-100" action="'. esc_url( home_url( '/' ) ) . '">
 			<input type="search" class="form-control search-field mw-100" placeholder="Suchbegriff..." value="" name="s">
             <div class="input-group-append">
-                <button class="btn btn-link" aria-label="Suchen">
+                <button class="input-group-btn" aria-label="Suchen">
                         <svg><use xmlns:xlink="http://www.w3.org/1999/xlink" href="'. get_stylesheet_directory_uri() .'/assets/img/phosphor-sprite.svg#magnifying-glass"></use></svg>
                 </button>
             </div>
