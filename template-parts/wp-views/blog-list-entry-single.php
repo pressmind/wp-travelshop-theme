0ca6fd5a148795c8ff99911d69bb14b0f3dc41b0
we craft <moin@wecraft.agency>
2023-08-15 13:59:27 +0200

    added default ratio settings

--- template-parts/wp-views/blog-list-entry-single.php
+++ template-parts/wp-views/blog-list-entry-single.php
@@ -5,6 +5,93 @@
         $post_thumbnail = get_the_post_thumbnail_url(get_the_ID(), 'large');
         ?>
 
+
+        <div class="blog-list-entry-header">
+
+            <h1 class="blog-list-entry-title">
+                <?php echo get_the_title(); ?>
+            </h1>
+
+            <?php
+            $post_categories = wp_get_post_categories(get_the_ID());
+            $post_tags = wp_get_post_tags(get_the_ID());
+            ?>
+
+            <div class="blog-list-entry-details small d-flex flex-row flex-wrap gap-2">
+
+                <?php
+                // Date
+                $post_date = get_the_date();
+                $post_date_link =  get_day_link(get_post_time('Y'), get_post_time('m'), get_post_time('j'));
+
+                $post_author_name = get_the_author_meta('display_name');
+
+                if ( $post_author_name ) {
+                    $post_author_id = get_the_author_meta('ID');
+                    $post_author_nicename = get_the_author_meta('user_nicename');
+                    $post_author_link = get_author_posts_url($post_author_id, $post_author_nicename);
+                }
+
+                $post_comments = get_comments( array('post_id' => get_the_ID() ) );
+
+                ?>
+
+
+                <?php if ( $post_date ) { ?>
+                    <div class="blog-list-entry-details-item">
+                        <div class="blog-list-entry-details-item-inner">
+                            <a href="<?php echo $post_date_link; ?>" title="<?php echo $post_date; ?>"><?php echo $post_date; ?></a>
+
+                            <?php
+                            if ( $post_categories ) {
+                                $post_categories_html = '';
+                                $iterate_cats = 0;
+                                foreach ( $post_categories as $category ) {
+                                    $post_category = get_category($category);
+
+                                    if ( $iterate_cats > 0 ) {
+                                        $post_categories_html .= ', ';
+                                    }
+                                    $post_categories_html .= '<a href="'.get_category_link( $post_category->term_id ).'" title="'.$post_category->name.'">'.$post_category->name.'</a>';
+
+                                    $iterate_cats++;
+
+                                }
+
+                                if ( !empty($post_categories_html) ) {
+                                    echo 'in ' . $post_categories_html;
+                                }
+                            }
+                            ?>
+                        </div>
+                    </div>
+                <?php } ?>
+
+                <?php if ( $post_author_name ) { ?>
+                    <div class="blog-list-entry-details-item"><div class="blog-list-entry-details-item-inner">Autor: <a href="<?php echo $post_author_link; ?>" title="<?php echo $post_author_name; ?>"><?php echo $post_author_name; ?></a></div></div>
+                <?php } ?>
+
+
+
+                <?php if ( $post_comments ) { ?>
+                    <div class="blog-list-entry-details-item">
+                        <div class="blog-list-entry-details-item-inner">
+                            <?php
+                            $post_comments_text = count($post_comments) . ' Kommentare';
+
+                            if ( count($post_comments) == 1 ) {
+                                $post_comments_text = count($post_comments) . ' Kommentar';
+                            }
+                            ?>
+                            <a href="<?php echo get_the_permalink(); ?>#post-comments" title="<?php echo $post_comments_text; ?>"><?php echo $post_comments_text; ?></a>
+                        </div>
+                    </div>
+                <?php } ?>
+
+            </div>
+
+        </div>
+
         <?php if ( $post_thumbnail ) { ?>
             <div class="blog-list-entry-thumbnail">
                 <div class="media-cover media-border-radius media-hover-scale ratio-16x9">
@@ -14,93 +101,6 @@
         <?php } ?>
 
         <div class="blog-list-entry-body">
-            <div class="blog-list-entry-header">
-
-                <h1 class="blog-list-entry-title">
-                        <?php echo get_the_title(); ?>
-                </h1>
-
-                <?php
-                $post_categories = wp_get_post_categories(get_the_ID());
-                $post_tags = wp_get_post_tags(get_the_ID());
-                ?>
-
-                <div class="blog-list-entry-details small d-flex flex-row flex-wrap gap-2">
-
-                    <?php
-                    // Date
-                    $post_date = get_the_date();
-                    $post_date_link =  get_day_link(get_post_time('Y'), get_post_time('m'), get_post_time('j'));
-
-                    $post_author_name = get_the_author_meta('display_name');
-
-                    if ( $post_author_name ) {
-                        $post_author_id = get_the_author_meta('ID');
-                        $post_author_nicename = get_the_author_meta('user_nicename');
-                        $post_author_link = get_author_posts_url($post_author_id, $post_author_nicename);
-                    }
-
-                    $post_comments = get_comments( array('post_id' => get_the_ID() ) );
-
-                    ?>
-
-
-                    <?php if ( $post_date ) { ?>
-                        <div class="blog-list-entry-details-item">
-                            <div class="blog-list-entry-details-item-inner">
-                                <a href="<?php echo $post_date_link; ?>" title="<?php echo $post_date; ?>"><?php echo $post_date; ?></a>
-
-                                <?php
-                                if ( $post_categories ) {
-                                    $post_categories_html = '';
-                                    $iterate_cats = 0;
-                                    foreach ( $post_categories as $category ) {
-                                        $post_category = get_category($category);
-
-                                        if ( $iterate_cats > 0 ) {
-                                            $post_categories_html .= ', ';
-                                        }
-                                        $post_categories_html .= '<a href="'.get_category_link( $post_category->term_id ).'" title="'.$post_category->name.'">'.$post_category->name.'</a>';
-
-                                        $iterate_cats++;
-
-                                    }
-
-                                    if ( !empty($post_categories_html) ) {
-                                        echo 'in ' . $post_categories_html;
-                                    }
-                                }
-                                ?>
-                            </div>
-                        </div>
-                    <?php } ?>
-
-                    <?php if ( $post_author_name ) { ?>
-                        <div class="blog-list-entry-details-item"><div class="blog-list-entry-details-item-inner">Autor: <a href="<?php echo $post_author_link; ?>" title="<?php echo $post_author_name; ?>"><?php echo $post_author_name; ?></a></div></div>
-                    <?php } ?>
-
-
-
-                    <?php if ( $post_comments ) { ?>
-                        <div class="blog-list-entry-details-item">
-                            <div class="blog-list-entry-details-item-inner">
-                                <?php
-                                $post_comments_text = count($post_comments) . ' Kommentare';
-
-                                if ( count($post_comments) == 1 ) {
-                                    $post_comments_text = count($post_comments) . ' Kommentar';
-                                }
-                                ?>
-                                <a href="<?php echo get_the_permalink(); ?>#post-comments" title="<?php echo $post_comments_text; ?>"><?php echo $post_comments_text; ?></a>
-                            </div>
-                       </div>
-                    <?php } ?>
-
-                </div>
-
-            </div>
-
-
             <div class="blog-list-entry-content">
                 <?php the_content(); ?>
             </div>
