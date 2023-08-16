98a1b2dce45a42b7b501e40ae35fd84b29ebe2e4
we craft <moin@wecraft.agency>
2023-08-16 16:14:19 +0200

    added default ratio settings

--- template-parts/micro-templates/link-sharing.php
+++ template-parts/micro-templates/link-sharing.php
@@ -3,7 +3,7 @@ $actual_link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https"
 // var_dump($actual_link);
 // @todo: build links to share pages with text
 
-if ( !isset ( $args['share_options'] ) ) {
+if ( !isset( $args['share_options'] ) ) {
     $args['share_options'] = [
         'title' => 'Reisetipp',
         'title_prefix' => 'Teile diese',
@@ -144,50 +144,3 @@ $share_title = !empty($args['share_options']['type']) ? $args['share_options']['
         </div>
     </div>
 </div>
-<?php /*
-<div class="share">
-    <label class="btn btn-primary" style="display:inline-flex; align-items: center;" for="sharemodal">
-        <svg><use xmlns:xlink="http://www.w3.org/1999/xlink" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/phosphor-sprite.svg#share-network"></use></svg>
-        </svg> <span style="margin-left:.5rem;">Teilen</span>
-    </label>
-    <input class="d-none" id="sharemodal" type="checkbox" />
-    <div class="share-box">
-        <ul>
-            <li>
-                <a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=<?php echo $actual_link; ?>">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-facebook" width="20" height="20" viewBox="0 0 24 24" stroke-width="2" stroke="#597e8d" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-                        <path d="M7 10v4h3v7h4v-7h3l1 -4h-4v-2a1 1 0 0 1 1 -1h3v-4h-3a5 5 0 0 0 -5 5v2h-3" />
-                    </svg> Facebook
-                </a>
-            </li>
-            <li class="d-md-none">
-                <a data-action="share/whatsapp/share" href="whatsapp://send?text=Ich%20empfehle%3A%2F%2F<?php echo $actual_link; ?>">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-whatsapp" width="20" height="20" viewBox="0 0 24 24" stroke-width="2" stroke="#597e8d" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-                        <path d="M3 21l1.65 -3.8a9 9 0 1 1 3.4 2.9l-5.05 .9" />
-                        <path d="M9 10a0.5 .5 0 0 0 1 0v-1a0.5 .5 0 0 0 -1 0v1a5 5 0 0 0 5 5h1a0.5 .5 0 0 0 0 -1h-1a0.5 .5 0 0 0 0 1" />
-                    </svg> WhatsApp
-                </a>
-            </li>
-            <li>
-                <a target="_blank" href="mailto:?subject=Reisetipp&amp;body=Ich%20empfehle%20die%20Reise%20<?php echo $actual_link; ?>">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-mail" width="20" height="20" viewBox="0 0 24 24" stroke-width="2" stroke="#597e8d" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-                        <rect x="3" y="5" width="18" height="14" rx="2" />
-                        <polyline points="3 7 12 13 21 7" />
-                    </svg> Mail
-                </a>
-            </li>
-            <li>
-                <a target="_blank" href="https://t.me/share/url?url=<?php echo $actual_link; ?>">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-telegram" width="20" height="20" viewBox="0 0 24 24" stroke-width="2" stroke="#597e8d" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-                        <path d="M15 10l-4 4l6 6l4 -16l-18 7l4 2l2 6l3 -4" />
-                    </svg> Telegram
-                </a>
-            </li>
-        </ul>
-    </div>
-</div>
- */ ?>
