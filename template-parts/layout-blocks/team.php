ec517a7559cba8024bccffad5fc27077333af6e7
we craft <moin@wecraft.agency>
2023-08-17 23:04:51 +0200

    added default ratio settings

--- template-parts/layout-blocks/team.php
+++ template-parts/layout-blocks/team.php
@@ -37,7 +37,7 @@
                         if ( !empty($item['image']) ) {
                             ?>
                             <div class="teaser-image">
-                                <div class="media media-cover media-circle ratio-1x1">
+                                <div class="media media-cover">
                                     <img alt="<?php echo $item['name']; ?>" loading="lazy" src="<?php echo $item['image']; ?>" />
                                 </div>
                             </div>
