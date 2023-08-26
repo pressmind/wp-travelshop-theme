ff97bf8d6d166c8e4a81f5f270aaaac74ae0f427
we craft <moin@wecraft.agency>
2023-08-26 21:59:09 +0200

    added default ratio settings

--- src/BeaverBuilderModuleLoader.php
+++ src/BeaverBuilderModuleLoader.php
@@ -59,6 +59,7 @@ class BeaverBuilderModuleLoader{
         require_once BB_MODULE_TS_DIR . 'modules/faq-schema-accordion/faq-schema-accordion.php';
 
         require_once BB_MODULE_TS_DIR . 'modules/ts-image-teaser/ts-image-teaser.php';
+        require_once BB_MODULE_TS_DIR . 'modules/ts-info-teaser/ts-info-teaser.php';
     }
 
     /**
